9daa147a98af1aa2200b7bf441727fe166812e71
Xixi R <sissisnothere@users.noreply.github.com>
2017-06-30 17:23:18 -0400

    using position-details component in the home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,43 +4,42 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 import { Component } from "@angular/core";
+import { Position } from "../position-detail/position";
+
 
 @Component({
+    selector: 'home',
     styleUrls: ['./home.component.scss'],
     templateUrl: './home.component.html',
 })
 
+
 export class HomeComponent {
 
-    //new instance of job position
+    
+    positions = POSITIONS;
+
+    selectedPosition: Position;
+    onSelect(position: Position): void {
+    	this.selectedPosition = position;
+    }
+
   
     position : Position = {
     	id: 0,
     	name: 'none'
     };
     
-
-    //create an array of position 
-
-    //a class member cannot have the 'const' keyword
-   // const POSITIONS : Position[] = [
-    POSITIONS : Position[] = [
-    	{ id: 11, name: "Software Developer"},
-    	{ id: 12, name: "System Engineer"},
-    	{ id: 13, name: "Validation Engineer"},
-    	{ id: 14, name: "Network Developer"},
-    	{ id: 15, name: "Security Engineer"},
-    	{ id: 16, name: "Database Administration"}
-    ]; 
-
-    positions = this.POSITIONS
    
-     
 }
 
-//this is the new class of job Position, has property id and name
-export class Position {
-	id : number;
-	name : string;
 
-}
+//a class member cannot have the 'const' keyword inside the class
+const POSITIONS : Position[] = [
+      { id: 11, name: "Software Developer"},
+      { id: 12, name: "System Engineer"},
+      { id: 13, name: "Validation Engineer"},
+      { id: 14, name: "Network Developer"},
+      { id: 15, name: "Security Engineer"},
+      { id: 16, name: "Database Administration"}
+]; 
