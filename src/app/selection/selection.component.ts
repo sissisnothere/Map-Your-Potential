abc40cdb9ec4af17ed60e423254555f4511abe44
Xixi R <sissisnothere@users.noreply.github.com>
2017-07-10 14:39:29 -0400

    move buttom from aboutComponent to selection component
    so modal window will be control in the selection component
    
    might be easier to change data in one component

--- src/app/selection/selection.component.ts
+++ src/app/selection/selection.component.ts
@@ -3,23 +3,68 @@
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 //if the file is in current folder/same level: need to have ./in front of the folder name
 import { Question } from "../selection/selection";
+import { QuestionService } from '../selection.service';
 
 @Component({
     selector: 'selection',
     styleUrls: ['./selection.component.scss'],
     templateUrl: './selection.component.html',
+    providers: [QuestionService]
 })
 
 //1. this is the new class of job Position, has property id and name
 // because we have a new class here /new component, need to add to app.module, otherwise error
-export class SelectionComponent {
+export class SelectionComponent implements OnInit {
 
 	//declare that hero is an input property, so data can do two way binding
 	@Input() question : Question;
 
+
+	questions : Question[];	//array
+	selectedQuestion: Question;	//single
+
+	/*
+	Mostly we use ngOnInit for all the initialization/declaration and avoid stuff to work in the constructor. The constructor should only be used to initialize class members but shouldn't do actual "work".
+	So you should use constructor() to setup Dependency Injection and not much else. ngOnInit() is better place to "start" - it's where/when components' bindings are resolved.
+	*/
+	constructor(private questionService : QuestionService) {
+
+	}
+
+	getQuestions(): void {
+      //this should return an array of positions, but if it complains: " Two different types with this name exist, but they are unrelated." 
+      //maybe bcause we import the position in two files, or missing import files from other location
+
+      // this.questionService.getQuestions().then(questions => this.questions=questions);
+
+      /*** this display the questions by the id ***/
+       this.selectedQuestion = this.questionService.getQuestions().find(x => x.id == 12);
+        console.log(this.selectedQuestion);  
+
+        //this.selectedQuestion = this.questions.;
+    } 
+
+
+    //if implements OnInit, must have the following ngOnInit function
+    //may not need it since it is called right after constructor?
+	ngOnInit(): void { 
+		this.getQuestions();
+	}
+
+	onOpen(open: Boolean): void {
+		this.open = open;
+		this.getQuestions();
+	}
+
+    open: Boolean = false;
+
+    onNext(): void {
+    	this.selectedQuestion = this.questionService.getQuestions().find(x => x.id == 11);
+        console.log(this.selectedQuestion);  
+    }
 }
 
 
